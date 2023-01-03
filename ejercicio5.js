472f812fefbd0f93ec9908d1238d863cdce4c594
SebaMen <sebastianmencia26@gmail.com>
2023-01-03 09:10:48 -0300

    Agrega ejercicio 5

---
+++ ejercicio5.js
@@ -0,0 +1,14 @@
+let alturaEnCm = parseInt(180)
+console.log(alturaEnCm)
+let alturaEnMts = parseFloat(1.80)
+console.log(alturaEnMts)
+let pesoEnKg = parseFloat(75.40)
+console.log(pesoEnKg)
+let redondeoAlturaEnMts = alturaEnMts.toFixed()
+console.log(redondeoAlturaEnMts)
+let redondeoParaAbajoPeso = Math.floor(pesoEnKg)
+console.log(redondeoParaAbajoPeso)
+let maximoValorJs = Number.MAX_VALUE
+if (maximoValorJs === maximoValorJs+1) {
+    console.log(maximoValorJs)
+}
---
+++ valueof.js
@@ -0,0 +1,8 @@
+// valueof: muestra el valor de una variable
+
+let numero1 = 4
+console.log(numero1.valueOf())
+ let numero = new Number (6)
+
+ console.log(numero.valueOf())
+
